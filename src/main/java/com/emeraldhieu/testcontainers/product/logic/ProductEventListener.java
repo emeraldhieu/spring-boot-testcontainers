7f9bfd8c065ee67d47bc7e3bef85137ee68918ba
Hieu <emerald.hieu@gmail.com>
2023-06-25 01:47:13 +0700

    Replace @EventListener with @TransactionalEventListener to keep event listener transactional

--- src/main/java/com/emeraldhieu/testcontainers/product/logic/ProductEventListener.java
+++ src/main/java/com/emeraldhieu/testcontainers/product/logic/ProductEventListener.java
@@ -4,10 +4,11 @@ import com.emeraldhieu.testcontainers.product.ProductMessage;
 import com.emeraldhieu.testcontainers.product.config.KafkaProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -26,7 +27,7 @@ public class ProductEventListener {
     private final KafkaProperties kafkaProperties;
     private final KafkaTemplate<String, ProductMessage> kafkaTemplate;
 
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handleProductCreated(ProductCreatedEvent event) {
         ProductMessage productMessage = ProductMessage.newBuilder()
             .setId(event.getId())
